4e27ffb6710620f85ad4f1ecf9f4a01ff97d65d8
Jacob Yundt <jyundt@gmail.com>
2015-09-15 00:00:56 -0400

    Adding a gitignore, refactoring the API again to return a better JSON top-level object.

--- python/flask/app.py
+++ python/flask/app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
-import pymongo
+from pymongo import MongoClient
 from flask import Flask, jsonify, request
 
 #define our mongo connection
-client = pymongo.MongoClient("localhost", 27017)
+client = MongoClient("localhost", 27017)
 db = client.oval
+races = db.races
 
-db.name
+
 
 app = Flask(__name__)
 
@@ -17,9 +18,11 @@ def index():
 @app.route('/categories')
 def get_categores():
 	if 'year' in request.args:
-		return 'Categories for ' + request.args['year']
+		#return 'Categories for ' + request.args['year']
+		return jsonify({'categories': races.distinct('category',{ "date": {"$gte": "new Date(2015,1,1)", "$lt": "new Date(2015,12,31)"}})})
 	else:
-		return 'Categories'	
+		#return 'Categories'	
+		return jsonify({'categories': races.distinct('category')})
 
 if __name__ == '__main__':
     app.run(debug=True)
