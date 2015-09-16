102c6a00368e32ef6b96521794fee2f1c2d51568
Jacob Yundt <jyundt@gmail.com>
2015-09-15 23:33:02 -0400

    Adding some error checking.

--- python/flask/app.py
+++ python/flask/app.py
@@ -17,21 +17,27 @@ def index():
 
 @app.route('/races')
 def get_races():
-	json_result = []
-	for race in db.races.find({},{"_id": 0}).limit(0):
-		json_result.append(race)
-	return jsonify({"races": json_result})
+    if 'year' in request.args and not request.args['year'].isdigit():
+        return jsonify({'code': 400, 'message': "Invalid year.", 'fields': 'year'}), 400
+
+    
+    json_result = []
+    for race in db.races.find({},{"_id": 0}).limit(0):
+        json_result.append(race)
+    return jsonify({"races": json_result})
 @app.route('/categories')
 def get_categores():
-	if 'year' in request.args:
-		#return 'Categories for ' + request.args['year']
-		return jsonify({'categories': races.distinct('category',{ "date": {"$gte": datetime.datetime(2015,1,1), "$lt": datetime.datetime(2015,12,31)}})})
-	else:
-		#return 'Categories'	
-		return jsonify({'categories': races.distinct('category')})
+    if 'year' in request.args and not request.args['year'].isdigit():
+        return jsonify({'code': 400, 'message': "Invalid year.", 'fields': 'year'}), 400
+    if 'year' in request.args:
+        start_date = datetime.datetime(int(request.args['year']),1,1)
+        end_date = datetime.datetime(int(request.args['year']),12,31)
+        return jsonify({'categories': races.distinct('category',{ "date": {"$gte": start_date, "$lt": end_date }})})
+    else:
+        return jsonify({'categories': races.distinct('category')})
 
 @app.route('/test')
 def test():
-	return "Value for year: " + request.args.get('year', 'HELLO!')
+    return "Value for year: " + request.args.get('year', 'HELLO!')
 if __name__ == '__main__':
     app.run(debug=True)
